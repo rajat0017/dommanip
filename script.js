84d6e4d7329d4223405c99ecdabff4f1a1ce28bd
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-05-23 14:27:07 +0530

    newupdates

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const items = document.getElementsByClassName('list-group-item');
-// items[1].style.backgroundColor='green';
-// items[2].style.visibility='hidden';
-var odd = document.querySelectorAll('li:nth-Child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='green';
-}
-var secondelem= document.querySelector('.list-group-item:nth-child(2)');
-secondelem.style.color='green';
+var itemlist= document.querySelector('#items');
+// itemlist.parentElement.style.backgroundColor= 'f4f4f4';
+
+// itemlist.children[1].style.backgroundColor='blue';
+// itemlist.firstElementChild.style.color='red';
+// itemlist.lastElementChild.style.fontWeight='bold';
+
+// itemlist.nextElementSibling.style.backgroundColor='#ccc';
+// itemlist.previousElementSibling.style.color='red';
+var newele= document.createElement('div');
+newele.className='new class';
+newele.id='newid';
+newele.setAttribute('title','hello');
+
+var newele_text= document.createTextNode('hello');
+newele.appendChild(newele_text);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newele,h1);
