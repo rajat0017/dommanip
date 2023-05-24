1d16558b6cc7d120259b3eaebb19640a0d61fa16
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-05-24 14:34:51 +0530

    task8

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-var itemlist= document.querySelector('#items');
-// itemlist.parentElement.style.backgroundColor= 'f4f4f4';
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
 
-// itemlist.children[1].style.backgroundColor='blue';
-// itemlist.firstElementChild.style.color='red';
-// itemlist.lastElementChild.style.fontWeight='bold';
+form.addEventListener('submit', addItem);
 
-// itemlist.nextElementSibling.style.backgroundColor='#ccc';
-// itemlist.previousElementSibling.style.color='red';
-var newele= document.createElement('div');
-newele.className='new class';
-newele.id='newid';
-newele.setAttribute('title','hello');
+itemList.addEventListener('click', removeItem);
 
-var newele_text= document.createTextNode('hello');
-newele.appendChild(newele_text);
+   
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-container.insertBefore(newele,h1);
+function addItem(e){
+   e.preventDefault();
+   var newItem= document.getElementById('item').value;
+   var li = document.createElement('li');
+   li.className='list-group-item';
+   
+   li.appendChild(document.createTextNode(newItem));
+   var deletebtn= document.createElement('button');
+   deletebtn.className='btn btn-danger btn-sm float-right delete margin-right: 16px';
+   deletebtn.appendChild(document.createTextNode('X'));
+   li.appendChild(deletebtn);
+   itemList.appendChild(li);
+
+//    var editbtn= document.createElement('button');
+//    editbtn.className='btn btn-success btn-sm float-right ';
+//    editbtn.appendChild(document.createTextNode('Edit'));
+//    li.appendChild(editbtn);
+
+
+   
+} 
+
+function removeItem(e){
+   if(e.target.classList.contains('delete'))  {
+    if(confirm('are you sure ?')){
+        var li= e.target.parentElement;
+        itemList.removeChild(li);
+    }
+   }  
+}
+
