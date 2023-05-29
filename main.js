83830c9e6283b5dffb94a77dd690a59f3e8f0a75
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-05-29 14:00:18 +0530

    edtbtn

--- main.js
+++ main.js
@@ -7,40 +7,46 @@ addstorage.addEventListener('submit', addtostorage);
 var container = document.getElementById('my-form');
 
 container.addEventListener('submit', additemlist);
-container.addEventListener('click', removeItem);
 
-function removeItem(myobj){
-    if(myobj.target.classList.contains('delete'))  {
-     if(confirm('are you sure ?')){
-         var ele= myobj.target.parentElement;
-        
-     
-         container.removeChild(ele);
-       
-         localStorage.removeItem(names.value);
-     }
-    } 
-
- }
-
-function additemlist(e) {
-    e.preventDefault();
+function additemlist(myobj) {
+    myobj.preventDefault();
 
     var li = document.createElement('li');
-    li.className = 'items';
-    li.id = 'itemid';
+    li.className = emails.value;
+    li.id = names.value;
     li.appendChild(document.createTextNode('Name=  ' +names.value +',  Email=' + emails.value));
     
     container.appendChild(li);
 
     var newbutton = document.createElement('button');
     newbutton.className='btn btn-danger btn-float-right delete';
+    
     newbutton.appendChild(document.createTextNode('delete'));
     li.appendChild(newbutton);
+    var editbtn = document.createElement('button');
+    editbtn.className='btn btn-danger btn-float-right delete';
+    editbtn.appendChild(document.createTextNode('edit'));
+    li.appendChild(editbtn);
     
 
-}
 
+newbutton.onclick=   ()  =>{
+  
+    container.removeChild(li);
+    console.log(li);
+
+    localStorage.removeItem(li.id);
+}
+editbtn.onclick = () => {
+    
+    document.getElementById('name').value=li.id;
+   document.getElementById('email').value=li.className;
+   
+   container.removeChild(li);
+    localStorage.removeItem(li.id);
+
+}
+}
 
 function addtostorage(e) {
 
